937148af63cfbc1ccaf536e10a773d8c64089e6d
Haroon Khan <HaroonKhan120704@gmail.com>
2023-07-31 00:03:31 +0100

    created nav

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [dropdownNav, setDropdownNav] = useState(false);
+
+  const hover = () => {
+    setDropdownNav(true);
+  };
+
+  const leave = () => {
+    setDropdownNav(false);
+  };
+
   return (
-    <div>
-      <h1>this is the main home page</h1>
-    </div>
+    <>
+      <nav className='flex p-2 border-2 border-white'>
+        <div>
+          <img
+            src='src/assets/images/logo.png'
+            alt=''
+            width='200px'
+            className='p-5'
+          />
+        </div>
+        <input
+          type='search'
+          name=''
+          id=''
+          placeholder='Search Item'
+          className='text-black w-3/6 ml-10 pl-10'
+        />
+        <ul className='flex justify-end'>
+          <li
+            className='p-5 cursor-pointer relative'
+            onMouseEnter={hover}
+            onMouseLeave={leave}
+          >
+            Browse
+            {dropdownNav && (
+              <ul className='absolute bg-white p-2 mt-2 text-black'>
+                <li className='hover:bg-neutral-400 cursor-pointer'>Shoes</li>
+                <li className='hover:bg-neutral-400 cursor-pointer'>
+                  Accessories
+                </li>
+                <li className='hover:bg-neutral-400 cursor-pointer'>
+                  Electronics
+                </li>
+              </ul>
+            )}
+          </li>
+          <li className='p-5 ml-auto'>Account: {'1'}</li>
+          <li className='p-5 cursor-pointer relative'>{'Basket: 🛒'}</li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
