deeeb2b2e7143929da9d2b1097aa43ed59c03ba2
Haroon Khan <HaroonKhan120704@gmail.com>
2023-08-05 02:02:27 +0100

    added jsx to electronic component and added back orignal jsx for main

--- src/Pages/Electronics.tsx
+++ src/Pages/Electronics.tsx
@@ -1,11 +1,90 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+
+const electronics = [
+  {
+    img: 'src/assets/images/ps5.png',
+    des: 'This is a Electronics',
+    lowPrice: 'Lowest Ask',
+    price: '£500',
+  },
+  {
+    img: 'src/assets/images/mouse.png',
+    des: 'This is a Electronics',
+    lowPrice: 'Lowest Ask',
+    price: '£200',
+  },
+  {
+    img: 'src/assets/images/airpods.png',
+    des: 'This is a Electronics',
+    lowPrice: 'Lowest Ask',
+    price: '£150',
+  },
+  {
+    img: 'src/assets/images/ps5.png',
+    des: 'This is Electronics',
+    lowPrice: 'Lowest Ask',
+    price: '£500',
+  },
+];
 
 const Electronics = () => {
   return (
-    <div>
+    <div className='flex flex-col'>
       <Nav />
-      <h1>This is the Electronics</h1>
+      <div className='flex justify-center'>
+        <div className='mt-40 p-16 w-8/12 bg-neutral-200'>
+          <h2 className='text-4xl'>Electronics</h2>
+          <p>
+            Buy and Sell Electronics on StockX. Every item is
+            <a className='text-green-900'> StockX Verified.</a>
+          </p>
+        </div>
+      </div>
+      <div className='flex justify-center flex-row mt-20'>
+        {electronics.map((image, index) => (
+          <div key={index} className='border border-neutral-400 m-10'>
+            <img
+              src={image.img}
+              alt={`Product ${index}`}
+              className='w-56 h-48 m-1 '
+            />
+            <p>{image.des}</p>
+            <p className='text-xs'>{image.lowPrice}</p>
+            <p className='font-bold text-xl'>{image.price}</p>
+          </div>
+        ))}
+      </div>
+      <div className='flex justify-center flex-row'>
+        {electronics.map((image, index) => (
+          <div key={index} className='border border-neutral-400 m-10'>
+            <img
+              src={image.img}
+              alt={`Product ${index}`}
+              className='w-56 h-48 m-1 '
+            />
+            <p>{image.des}</p>
+            <p className='text-xs'>{image.lowPrice}</p>
+            <p className='font-bold text-xl'>{image.price}</p>
+          </div>
+        ))}
+      </div>
+      <div className='flex justify-center flex-row'>
+        {electronics.map((image, index) => (
+          <div key={index} className='border border-neutral-400 m-10'>
+            <img
+              src={image.img}
+              alt={`Product ${index}`}
+              className='w-56 h-48 m-1 '
+            />
+            <p>{image.des}</p>
+            <p className='text-xs'>{image.lowPrice}</p>
+            <p className='font-bold text-xl'>{image.price}</p>
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 };
