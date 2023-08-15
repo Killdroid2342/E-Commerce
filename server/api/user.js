b2f630d03b2cabb77b6fe14e4f1bfa0312634275
Haroon Khan <HaroonKhan120704@gmail.com>
2023-08-15 14:09:22 +0100

    created register post

--- server/api/user.js
+++ server/api/user.js
@@ -3,8 +3,24 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const { createUser, isUserExists, hashPassword } = require('../modal/user');
+
 router.use(bodyParser.json());
 
-router.post('/register-user', async (req, res) => {});
+router.post('/register-user', async (req, res) => {
+  const { username, password } = req.body;
+  const saltRounds = 12;
+  const hashPasswordRes = await hashPassword(password, saltRounds);
+  if ((await isUserExists(username)) == false) {
+    createUser(username, hashPasswordRes);
+    res.send({
+      message: 'Users created successfully',
+    });
+  } else {
+    res.send({
+      message: 'Username already exists',
+    });
+  }
+});
 
 module.exports = router;
