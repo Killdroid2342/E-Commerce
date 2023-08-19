eb04e5f87072bc7d478d6313dffb4e3dac201835
Haroon Khan <HaroonKhan120704@gmail.com>
2023-08-20 00:14:42 +0100

    added files

--- src/Pages/Payment.tsx
+++ src/Pages/Payment.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import PaymentForm from '../components/PaymentForm';
+import ShippingForm from '../components/Checkout/ShippingForm';
 
 const Payment = () => {
-  return <div></div>;
+  return (
+    <div className='otherBGColor'>
+      <ShippingForm />
+      <PaymentForm />
+    </div>
+  );
 };
 
 export default Payment;
