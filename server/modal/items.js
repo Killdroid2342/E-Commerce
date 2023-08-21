71e9c460f13406164c36bcbebf701efb1876c72d
Haroon Khan <HaroonKhan120704@gmail.com>
2023-08-22 00:01:24 +0100

    get items in database

---
+++ server/modal/items.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ecommerce',
+});
+
+const getshoeitems = async () => {
+  const res = conn
+    .promise()
+    .query('SELECT * FROM shoeitems')
+    .then(([rows, fields]) => {
+      return rows;
+    });
+  return res;
+};
+const getelectronicitems = async () => {
+  const res = conn
+    .promise()
+    .query('SELECT * FROM electronicitems')
+    .then(([rows, fields]) => {
+      return rows;
+    });
+  return res;
+};
+const getaccessoriesitems = async () => {
+  const res = conn
+    .promise()
+    .query('SELECT * FROM accessoriesitems')
+    .then(([rows, fields]) => {
+      return rows;
+    });
+  return res;
+};
+module.exports = {
+  getshoeitems,
+  getelectronicitems,
+  getaccessoriesitems,
+};
