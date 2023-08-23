4812766c27bc03ba33789016607171bd9871e1c0
Haroon Khan <HaroonKhan120704@gmail.com>
2023-08-24 00:32:36 +0100

    added more items on main page

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Nav from '../components/nav/Nav';
 import Footer from '../components/Footer';
 import { useNavigate } from 'react-router-dom';
 import Auth from '../hooks/Auth';
+const { VITE_API_URL } = import.meta.env;
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: VITE_API_URL,
+});
+interface Items {
+  img: string;
+  des: string;
+  lowPrice: string;
+  price: string;
+  name: string;
+}
+interface Main {
+  name: string;
+  img: string;
+}
 const carouselImages = [
   'src/assets/images/dummyImage1.png',
   'src/assets/images/dummyImage2.png',
@@ -10,27 +27,27 @@ const carouselImages = [
 
 const productImages = [
   {
-    name: 'this is an image of the shoe1',
+    name: 'Yeezy',
     img: 'src/assets/images/shoe1.png',
   },
   {
-    name: 'this is an image of the shoe2',
+    name: 'Jordan',
     img: 'src/assets/images/LatestShoe2.png',
   },
   {
-    name: 'this is an image of the shoe1',
+    name: 'Yeezy',
     img: 'src/assets/images/shoe1.png',
   },
   {
-    name: 'this is an image of the shoe2',
+    name: 'Jordan',
     img: 'src/assets/images/LatestShoe2.png',
   },
   {
-    name: 'this is an image of the shoe1',
+    name: 'Yeezy',
     img: 'src/assets/images/shoe1.png',
   },
   {
-    name: 'this is an image of the shoe2',
+    name: 'Jordan',
     img: 'src/assets/images/LatestShoe2.png',
   },
 ];
@@ -63,20 +80,45 @@ const brands = [
 const igImage = [
   {
     img: 'src/assets/images/igImage1.png',
+    name: 'Instagram Images',
   },
   {
     img: 'src/assets/images/igImage2.png',
+    name: 'Instagram Images',
   },
   {
     img: 'src/assets/images/igImage3.png',
+    name: 'Instagram Images',
   },
   {
     img: 'src/assets/images/igImage4.png',
+    name: 'Instagram Images',
+  },
+];
+const LatestShoes = [
+  {
+    img: 'src/assets/images/LatestShoe3.png',
+    name: 'Jordans',
+  },
+  {
+    img: 'src/assets/images/LatestShoe2.png',
+    name: 'Jordans',
+  },
+  {
+    img: 'src/assets/images/LatestShoe.png',
+    name: 'Jordans',
+  },
+  {
+    img: 'src/assets/images/jordan4.png',
+    name: 'Jordans',
   },
 ];
 const Home = () => {
   Auth();
   const [img, setImg] = useState(0);
+  const [accessories, setAccessories] = useState<Items[]>([]);
+  const [main, setMain] = useState<Main[]>([]);
+  console.log(main);
   const navigate = useNavigate();
   const preivImg = () => {
     setImg((prevImg) => (prevImg + 1) % carouselImages.length);
@@ -87,6 +129,18 @@ const Home = () => {
       prevImg === 0 ? carouselImages.length - 1 : prevImg - 1
     );
   };
+  async function getAccessories() {
+    const res = await instance.get('/items/getaccessoriesitems');
+    setAccessories(res.data);
+  }
+  async function getMainItems() {
+    const res = await instance.get('/items/getMainItems');
+    setMain(res.data);
+  }
+  useEffect(() => {
+    getAccessories();
+    getMainItems();
+  }, []);
   return (
     <>
       <Nav />
@@ -96,7 +150,7 @@ const Home = () => {
             {'<'}
           </button>
           <img
-            className='w-9/12'
+            className='w-9/12 h-96'
             src={carouselImages[img]}
             alt={`Carousel Slide ${img}`}
           />
@@ -106,27 +160,25 @@ const Home = () => {
         </div>
         <p className='font-bold ml-80 mt-10'>Recommended For You</p>
         <div className='flex justify-center'>
-          <div className='flex justify-center'>
-            {productImages.map((image, index) => (
-              <div key={index} className='relative group'>
-                <img
-                  src={image.img}
-                  alt={`Product ${index}`}
-                  className='w-56 h-48 m-1 border border-neutral-400 transition duration-300 ease-in-out transform group-hover:scale-105'
-                />
-                <div className='absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-80 backdrop-blur-md transition-opacity duration-300'>
-                  <div
-                    onClick={() => navigate('/shoes')}
-                    className='bg-black text-white text-center p-2 rounded-md'
-                  >
-                    <p className='cursor-pointer'>
-                      Click here to go to shoes page
-                    </p>
-                  </div>
+          {productImages.map((image, index) => (
+            <div key={index} className='relative group'>
+              <img
+                src={image.img}
+                alt={`Product ${index}`}
+                className='w-56 h-48 m-1 border border-neutral-400 transition duration-300 ease-in-out transform group-hover:scale-105'
+              />
+              <div className='absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-80 backdrop-blur-md transition-opacity duration-300'>
+                <div
+                  onClick={() => navigate('/shoes')}
+                  className='bg-black text-white text-center p-2 rounded-md'
+                >
+                  <p className='cursor-pointer'>
+                    Click here to go to shoes page
+                  </p>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
         <p className='font-bold ml-80 mt-10'>Popular Brands</p>
         <div className='flex justify-center'>
@@ -141,13 +193,29 @@ const Home = () => {
             </div>
           ))}
         </div>
-        <div className='border border-neutral-400 rounded-md m-5  flex flex-col items-center justify-center'>
-          <p className='font-bold'>Get The Latest Shoes</p>
-          <div className='flex flex-row'>
-            <img className='' src='src/assets/images/LatestShoe3.png' alt='' />
-            <img className='' src='src/assets/images/LatestShoe2.png' alt='' />
-            <img className='' src='src/assets/images/LatestShoe.png' alt='' />
-          </div>
+        <p className='font-bold ml-80 mt-10'>Get The Latest Shoes</p>
+        <div className='flex justify-center'>
+          {LatestShoes.map((image, index) => (
+            <div key={index} className='flex flex-col items-center'>
+              <img
+                src={image.img}
+                alt={'shoe Images'}
+                className='w-72 h-60 m-10 border border-neutral-400'
+              />
+            </div>
+          ))}
+        </div>
+        <p className='font-bold ml-80 mt-10'>Get The Latest Items</p>
+        <div className='flex justify-center'>
+          {accessories.slice(0, 4).map((image, index) => (
+            <div key={index} className='flex flex-col items-center'>
+              <img
+                src={image.img}
+                alt={'shoe Images'}
+                className='w-72 h-60 m-10 border border-neutral-400'
+              />
+            </div>
+          ))}
         </div>
         <p className='font-bold ml-80 mt-10'>As Seen On Instagram</p>
         <div className='flex justify-center'>
