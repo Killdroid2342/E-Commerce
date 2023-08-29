941a8492d9aac85509b0723657e57e72f5904ab3
Haroon Khan <HaroonKhan120704@gmail.com>
2023-08-30 00:05:08 +0100

    created endpoint

--- server/api/items.js
+++ server/api/items.js
@@ -16,7 +16,12 @@ router.get('/allItems', async (req, res) => {
   const totalItems = await getItems();
   res.send(JSON.stringify(totalItems));
 });
-router.post('/search', async (req, res) => {});
+router.post('/search', async (req, res) => {
+  const { searchItem } = req.body;
+  console.log(searchItem);
+  const matchingItems = await searchItems(searchItem);
+  res.status(200).send(matchingItems);
+});
 router.get('/addToDatabase', async (req, res) => {
   const data = [{}];
   data.map((index) => {
