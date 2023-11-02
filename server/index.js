25be5bc2645ba92b451648101bb3237183ea154d
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-02 22:16:32 +0000

    changed cors origin

--- server/index.js
+++ server/index.js
@@ -7,10 +7,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const routes = require('./api/index');
 
 const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://e-commerce-killdroid2342.vercel.app/',
-  ],
+  origin: ['http://localhost:5173', 'https://e-commerce-wine-mu.vercel.app/'],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
