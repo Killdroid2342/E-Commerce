20e94bcff88df10e7f0c8607b4473b4669f3e7af
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-03 00:02:47 +0000

    removed useless duplicate url

--- server/index.js
+++ server/index.js
@@ -7,11 +7,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const routes = require('./api/index');
 
 const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://e-commerce-wine-mu.vercel.app/',
-    'https://e-commerce-wine-mu.vercel.app',
-  ],
+  origin: ['http://localhost:5173', 'https://e-commerce-wine-mu.vercel.app/'],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
