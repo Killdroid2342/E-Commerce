7ce6dd4ce97ddccfc3b075de98fa17705fd59ced
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-16 00:11:49 +0000

    deleted modal

--- src/Pages/CreateCard.tsx
+++ src/Pages/CreateCard.tsx
@@ -8,7 +8,6 @@ import Card from '../components/Card';
 
 const CreateCard = () => {
   const [creditCard, setCreditCard] = useState({});
-  const [cardForm, setCardForm] = useState(false);
   const initialBasketItems = localStorage.getItem('basketItems');
   const [basketItems, setBasketItems] = useState<Item[]>(
     initialBasketItems ? JSON.parse(initialBasketItems) : []
@@ -26,9 +25,7 @@ const CreateCard = () => {
       [name]: value,
     }));
   }
-  const openCardForm = () => {
-    setCardForm(!cardForm);
-  };
+
   useEffect(() => {
     localStorage.setItem('basketItems', JSON.stringify(basketItems));
   }, [basketItems]);
@@ -37,25 +34,22 @@ const CreateCard = () => {
     <>
       <Nav basketItems={basketItems} setBasketItems={setBasketItems} />
       <h1 className='text-center p-2 text-2xl mt-5'>Create Your Own Card</h1>
-      <div className='flex flex-col justify-center items-center mt-24'>
-        <div className='border border-black w-96 h-96'>
+      <h2 className='text-center p-2 text-2xl mt-5'>
+        Disclaimer: Do Not Put any real credit card details inside of these
+        forms. This is only a demo and make your own details up.
+      </h2>
+      <div className='flex flex-row justify-center items-center mt-24'>
+        <div className='border border-black w-96 h-96 mr-4'>
+          {' '}
           <Canvas>
             <ambientLight />
             <pointLight position={[5, 5, 5]} />
             <Card />
           </Canvas>
         </div>
-        <p onClick={openCardForm} className='cursor-pointer mt-20 text-2xl'>
-          Edit Card
-        </p>
-        {cardForm && (
-          <CardForm
-            submitCard={submitCard}
-            cardInput={cardInput}
-            cardForm={cardForm}
-            setCardForm={setCardForm}
-          />
-        )}
+        <div className='w-96'>
+          <CardForm submitCard={submitCard} cardInput={cardInput} />
+        </div>
       </div>
     </>
   );
