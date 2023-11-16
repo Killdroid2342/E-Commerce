2120292d0e8a522559e3cd47fd058492b4e3ded7
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-16 00:11:30 +0000

    css changes

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,81 +1,90 @@
-const CardForm = ({ submitCard, cardInput, cardForm, setCardForm }: any) => {
-  const closeCardForm = () => {
-    setCardForm(!cardForm);
-  };
-
+const CardForm = ({ submitCard, cardInput }: any) => {
   return (
-    <>
-      <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black'>
-        <div className='bg-white p-8 rounded-lg overflow-auto max-h-96 w-96'>
-          <button
-            onClick={closeCardForm}
-            className='absolute top-2 right-2 px-3 py-2 bg-gray-500 text-white rounded-lg mt-40'
-          >
-            X
-          </button>
-          <form
-            className='border border-red-900 flex flex-col'
-            onSubmit={submitCard}
-          >
-            <label>First Name: </label>
-            <input
-              placeholder='John'
-              type='text'
-              required
-              maxLength={16}
-              className='border border-neutral-500 focus:outline-none'
-              name='FirstName'
-              onChange={cardInput}
-            />
-            <label>Last Name: </label>
-            <input
-              placeholder='Doe'
-              type='text'
-              required
-              maxLength={16}
-              className='border border-neutral-500 focus:outline-none'
-              name='LastName'
-              onChange={cardInput}
-            />
-            <label>Card Number: </label>
-            <input
-              placeholder='1234 1234 1234 1234'
-              type='text'
-              required
-              maxLength={16}
-              className='border border-neutral-500 focus:outline-none'
-              name='cardNumber'
-              onChange={cardInput}
-            />
-            <label>Expiration Date: </label>
-            <input
-              type='text'
-              placeholder='10/25'
-              maxLength={5}
-              required
-              className='border border-neutral-500 focus:outline-none'
-              name='expirationDate'
-              onChange={cardInput}
-            />
-            <label>Security Code: </label>
-            <input
-              type='text'
-              placeholder='123'
-              maxLength={3}
-              required
-              className='border border-neutral-500 focus:outline-none'
-              name='securityCode'
-              onChange={cardInput}
-            />
-            <input
-              type='submit'
-              value='Create Card'
-              className='border border-black rounded-lg'
-            />
-          </form>
+    <div className='flex flex-col items-center'>
+      <form className='border border-red-900 p-4 w-96' onSubmit={submitCard}>
+        <div className='mb-2 flex flex-col'>
+          <label>First Name:</label>
+          <input
+            type='text'
+            placeholder='John'
+            required
+            maxLength={16}
+            className='border border-neutral-500 p-2 focus:outline-none'
+            name='FirstName'
+            onChange={cardInput}
+          />
         </div>
-      </div>
-    </>
+
+        <div className='mb-2 flex flex-col'>
+          <label>Last Name:</label>
+          <input
+            type='text'
+            placeholder='Doe'
+            required
+            maxLength={16}
+            className='border border-neutral-500 p-2 focus:outline-none'
+            name='LastName'
+            onChange={cardInput}
+          />
+        </div>
+
+        <div className='mb-2 flex flex-col'>
+          <label>Card Number:</label>
+          <input
+            type='text'
+            placeholder='1234 1234 1234 1234'
+            required
+            maxLength={16}
+            className='border border-neutral-500 p-2 focus:outline-none'
+            name='CardNumber'
+            onChange={cardInput}
+          />
+        </div>
+
+        <div className='mb-2 flex flex-col'>
+          <label>Expiration Date:</label>
+          <input
+            type='text'
+            placeholder='10/25'
+            maxLength={5}
+            required
+            className='border border-neutral-500 p-2 focus:outline-none'
+            name='ExpirationDate'
+            onChange={cardInput}
+          />
+        </div>
+
+        <div className='mb-2 flex flex-col'>
+          <label>Security Code:</label>
+          <input
+            type='text'
+            placeholder='123'
+            maxLength={3}
+            required
+            className='border border-neutral-500 p-2 focus:outline-none'
+            name='SecurityCode'
+            onChange={cardInput}
+          />
+        </div>
+        <div className='mb-2 flex flex-col'>
+          <label>Add Money:</label>
+          <input
+            type='number'
+            placeholder='£0'
+            required
+            className='border border-neutral-500 p-2 focus:outline-none appearance-none'
+            name='Money'
+            onChange={cardInput}
+          />
+        </div>
+
+        <input
+          type='submit'
+          value='Create Card'
+          className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700'
+        />
+      </form>
+    </div>
   );
 };
 
