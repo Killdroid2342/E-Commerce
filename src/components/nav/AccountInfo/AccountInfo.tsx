bcbd19fa6524f5d367d6e87db8faf41462ca4ae4
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-17 13:33:16 +0000

    getting card info

--- src/components/nav/AccountInfo/AccountInfo.tsx
+++ src/components/nav/AccountInfo/AccountInfo.tsx
@@ -1,38 +1,81 @@
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
+const { VITE_API_URL } = import.meta.env;
+interface Account {
+  account: string;
+  Money: string;
+  CardNumber: string;
+  ExpirationDate: string;
+  SecurityCode: string;
+  FullName: string;
+}
 const AccountInfo = ({ setAccountinfo, clientUsername }: any) => {
+  const [accountInfo, setAccountInfo] = useState<Account[]>([]);
+  console.log(accountInfo);
+  const instance = axios.create({
+    baseURL: VITE_API_URL,
+  });
+
   const navigate = useNavigate();
 
   const closeModal = () => {
     setAccountinfo(false);
   };
-
+  async function getAccountInfo() {
+    const res = await instance.get('/card/accountInfo');
+    setAccountInfo(res.data);
+  }
+  useEffect(() => {
+    getAccountInfo();
+  }, []);
   return (
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black'>
       <div className='bg-white p-8 rounded-lg overflow-auto max-h-96 w-96'>
         <button
-          className='absolute top-2 right-2 px-3 py-2 bg-gray-500 text-white rounded-lg'
+          className='p-2 px-3 bg-gray-500 text-white rounded-lg'
           onClick={closeModal}
         >
           X
         </button>
         <h2 className='text-2xl font-semibold mb-4'>Account Information</h2>
         <div className='grid gap-4'>
-          <p className='font-bold'>Account Name: {clientUsername}</p>
-          <p>Balance: </p>
-          <p>Card Number: </p>
-          <p>Expiration Date: </p>
-          <p>Security Code: </p>
-          <p>
-            No card? Click{' '}
-            <span
-              onClick={() => navigate('/CreateCard')}
-              className='text-blue-500 cursor-pointer'
-            >
-              Here
-            </span>{' '}
-            to create a card.
-          </p>
+          {accountInfo.map((account) => {
+            if (clientUsername === account.account) {
+              return (
+                <div key={account.account}>
+                  <p className='font-bold'>Account Name: {clientUsername}</p>
+                  <p>Full Name: {account.FullName}</p>
+                  <p>{`Balance: £${account.Money}`}</p>
+                  <p>Card Number: {account.CardNumber}</p>
+                  <p>Expiration Date: {account.ExpirationDate}</p>
+                  <p>Security Code: {account.SecurityCode}</p>
+                  <p>
+                    View Your Card{' '}
+                    <span
+                      onClick={() => navigate('/CreateCard')}
+                      className='text-blue-500 cursor-pointer'
+                    >
+                      Here
+                    </span>
+                  </p>
+                </div>
+              );
+            }
+            return (
+              <p>
+                No card? Click{' '}
+                <span
+                  onClick={() => navigate('/CreateCard')}
+                  className='text-blue-500 cursor-pointer'
+                >
+                  Here
+                </span>{' '}
+                to create a card.
+              </p>
+            );
+          })}
         </div>
       </div>
     </div>
