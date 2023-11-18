748a1b5b76f2549fd6cf30fa44e778a77a04fc96
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-18 00:02:57 +0000

    created instance

--- src/Pages/CreateCard.tsx
+++ src/Pages/CreateCard.tsx
@@ -3,17 +3,66 @@ import Nav from '../components/nav/Nav';
 import CardForm from '../components/CardForm';
 import { Item } from './Shoes';
 import { Canvas } from '@react-three/fiber';
-
+const { VITE_API_URL } = import.meta.env;
 import Card from '../components/Card';
-
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { decodeToken } from 'react-jwt';
+import ResModal from '../components/Forms/Form-Modal/ResModal';
+interface CreditCard {
+  CardNumber: string;
+  ExpirationDate: string;
+  FullName: string;
+  Money: string;
+  SecurityCode: string;
+}
 const CreateCard = () => {
-  const [creditCard, setCreditCard] = useState({});
+  const [creditCard, setCreditCard] = useState<CreditCard>({
+    CardNumber: '',
+    ExpirationDate: '',
+    FullName: '',
+    Money: '',
+    SecurityCode: '',
+  });
   const initialBasketItems = localStorage.getItem('basketItems');
   const [basketItems, setBasketItems] = useState<Item[]>(
     initialBasketItems ? JSON.parse(initialBasketItems) : []
   );
-  function submitCard(e: React.FormEvent) {
+  const [modal, setModal] = useState(false);
+
+  const [clientUsername, setClientUsername] = useState('');
+  const instance = axios.create({
+    baseURL: VITE_API_URL,
+  });
+  console.log(clientUsername);
+  const usernameJWT = () => {
+    const getJWT = Cookies.get('UserjwtToken');
+    if (getJWT) {
+      const decodedTokenUsername = (decodeToken(getJWT) as { username: string })
+        .username;
+      setClientUsername(decodedTokenUsername);
+    } else return;
+  };
+  useEffect(() => {
+    usernameJWT();
+  }, []);
+  async function submitCard(e: React.FormEvent) {
     e.preventDefault();
+    const res = await instance.post('/card/uploadDetails', {
+      CardNumber: creditCard.CardNumber,
+      ExpirationDate: creditCard.ExpirationDate,
+      FullName: creditCard.FullName,
+      Money: creditCard.Money,
+      SecurityCode: creditCard.SecurityCode,
+      account: clientUsername,
+    });
+    setModal(res.data.message);
+
+    setTimeout(() => {
+      setModal(false);
+    }, 2000);
+
+    console.log(res, 'THIS IS RES');
     console.log('FORM CLICKED');
     console.log(creditCard);
   }
@@ -33,6 +82,7 @@ const CreateCard = () => {
   return (
     <>
       <Nav basketItems={basketItems} setBasketItems={setBasketItems} />
+      {modal !== false ? <ResModal responseMessage={modal} /> : ''}
       <h1 className='text-center p-2 text-2xl mt-5'>Create Your Own Card</h1>
       <h2 className='text-center p-2 text-2xl mt-5'>
         Disclaimer: Do Not Put any real credit card details inside of these
