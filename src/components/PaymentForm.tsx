a4f56b63cbe616fb82f12f7076c158b44c4e4885
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-20 02:28:14 +0000

    nearly completed payment

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,41 +1,101 @@
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-const PaymentForm = () => {
-  const navigate = useNavigate();
-  const [isChecked, setIsChecked] = useState(true);
+// import { useNavigate } from 'react-router-dom';
 
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  const years = [
-    '2023',
-    '2024',
-    '2025',
-    '2026',
-    '2027',
-    '2028',
-    '2029',
-    '2030',
-    '2031',
-    '2032',
-    '2033',
-    '2034',
-    '2035',
-    '2036',
-    '2037',
-    '2038',
-  ];
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const { VITE_API_URL } = import.meta.env;
+interface Account {
+  account: string;
+  Money: string;
+  CardNumber: string;
+  ExpirationDate: string;
+  SecurityCode: string;
+  FullName: string;
+}
+
+const PaymentForm = () => {
+  // const navigate = useNavigate();
+  const [isChecked, setIsChecked] = useState(true);
+  const [accountInfo, setAccountInfo] = useState<Account[]>([]);
+  const [formData, setFormData] = useState({
+    FullName: '',
+    CardNumber: '',
+    ExpirationDate: '',
+    SecurityCode: '',
+  });
+  console.log(accountInfo, 'this is accoutn info');
+  console.log(accountInfo[0]?.CardNumber, 'this is accoutn info');
+  const instance = axios.create({
+    baseURL: VITE_API_URL,
+  });
+  async function getAccountInfo() {
+    const res = await instance.get('/card/accountInfo');
+    setAccountInfo(res.data);
+  }
+  useEffect(() => {
+    getAccountInfo();
+  }, []);
+  const storedItems = localStorage.getItem('basketItems');
+  const items = storedItems ? JSON.parse(storedItems) : [];
+  console.log(items, 'these are the items');
+
+  if (items.length > 0) {
+    items.forEach((item: any, index: any) => {
+      console.log(
+        `Item ${index + 1}:`,
+        item.name,
+        item.price,
+        item.shoe,
+        item.amount,
+        accountInfo[0]?.account,
+        'this is the data sent to the database'
+      );
+    });
+  } else {
+    console.log('No items in the array');
+  }
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+  const submitPayment = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData, 'this is form data');
+
+    if (
+      formData.CardNumber === accountInfo[0]?.CardNumber &&
+      formData.FullName === accountInfo[0]?.FullName &&
+      formData.ExpirationDate === accountInfo[0]?.ExpirationDate &&
+      formData.SecurityCode === accountInfo[0]?.SecurityCode
+    ) {
+      const storedItems = localStorage.getItem('basketItems');
+      const items = storedItems ? JSON.parse(storedItems) : [];
+
+      try {
+        await Promise.all(
+          items.map(async (item: any) => {
+            const res = await instance.post('/items/purchasedItems', {
+              name: item.name,
+              price: item.price,
+              shoe: item.shoe,
+              amount: item.amount,
+              account: accountInfo[0]?.account,
+            });
+            console.log(res, 'this is res for item:', item);
+          })
+        );
+        console.log('All purchases successful');
+        localStorage.removeItem('basketItems');
+        // navigate('/PurchasedItems');
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log('Incorrect payment details');
+    }
+  };
 
   return (
     <>
@@ -65,6 +125,8 @@ const PaymentForm = () => {
               className='w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors'
               placeholder='John Doe'
               type='text'
+              onChange={handleInputChange}
+              name='FullName'
             />
           </div>
           <div className='mb-3'>
@@ -74,33 +136,22 @@ const PaymentForm = () => {
               placeholder='0000 0000 0000 0000'
               type='text'
               maxLength={16}
+              onChange={handleInputChange}
+              name='CardNumber'
             />
           </div>
-          <div className='mb-3 -mx-2 flex items-end'>
-            <div className='px-2 w-1/2'>
-              <label className='font-bold text-sm mb-2 ml-1'>
-                Expiration date
-              </label>
-
-              <select className='form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer'>
-                {months.map((allMonths, key) => {
-                  return (
-                    <option key={key} value={allMonths}>
-                      {allMonths}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className='px-2 w-1/2'>
-              <select className='form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer'>
-                {years.map((allYears, key) => (
-                  <option key={key} value={allYears}>
-                    {allYears}
-                  </option>
-                ))}
-              </select>
-            </div>
+          <div className='mb-3'>
+            <label className='font-bold text-sm mb-2 ml-1'>
+              Expiration date
+            </label>
+            <input
+              className='w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors'
+              placeholder='10/25'
+              type='text'
+              maxLength={5}
+              name='ExpirationDate'
+              onChange={handleInputChange}
+            />
           </div>
           <div className='mb-10 flex flex-col'>
             <label className='font-bold text-sm mb-2 ml-1'>Security code</label>
@@ -109,6 +160,8 @@ const PaymentForm = () => {
               placeholder='000'
               type='text'
               maxLength={3}
+              onChange={handleInputChange}
+              name='SecurityCode'
             />
           </div>
           <div>
