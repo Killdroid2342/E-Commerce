0a5dabf515f77bf8ad93c0bde95705bea62a735a
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-21 17:00:12 +0000

    created button

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,53 +1,46 @@
-const CardForm = ({ submitCard, cardInput, removeCard }: any) => {
+const CardForm = ({ submitCard, cardInput, removeCard, addMoney }: any) => {
   return (
     <div className='flex flex-col items-center'>
-      <form className='border border-red-900 p-4 w-96' onSubmit={submitCard}>
+      <form className='border border-red-900 p-4 ml-10' onSubmit={submitCard}>
         <div className='mb-2 flex flex-col'>
           <label>FullName:</label>
           <input
             type='text'
             placeholder='John Doe'
-            required
             maxLength={16}
             className='border border-neutral-500 p-2 focus:outline-none'
             name='FullName'
             onChange={cardInput}
           />
         </div>
-
         <div className='mb-2 flex flex-col'>
           <label>Card Number:</label>
           <input
             type='text'
             placeholder='1234 1234 1234 1234'
-            required
             maxLength={16}
             className='border border-neutral-500 p-2 focus:outline-none'
             name='CardNumber'
             onChange={cardInput}
           />
         </div>
-
         <div className='mb-2 flex flex-col'>
           <label>Expiration Date:</label>
           <input
             type='text'
             placeholder='10/25'
             maxLength={5}
-            required
             className='border border-neutral-500 p-2 focus:outline-none'
             name='ExpirationDate'
             onChange={cardInput}
           />
         </div>
-
         <div className='mb-2 flex flex-col'>
           <label>Security Code:</label>
           <input
             type='text'
             placeholder='123'
             maxLength={3}
-            required
             className='border border-neutral-500 p-2 focus:outline-none'
             name='SecurityCode'
             onChange={cardInput}
@@ -58,7 +51,6 @@ const CardForm = ({ submitCard, cardInput, removeCard }: any) => {
           <input
             type='number'
             placeholder='£0'
-            required
             className='border border-neutral-500 p-2 focus:outline-none appearance-none'
             name='Money'
             onChange={cardInput}
@@ -68,13 +60,19 @@ const CardForm = ({ submitCard, cardInput, removeCard }: any) => {
           <input
             type='submit'
             value='Create Card'
-            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700'
+            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
           />
           <input
             onClick={removeCard}
             type='submit'
             value='Remove Card'
-            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700 ml-2'
+            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
+          />
+          <input
+            onClick={addMoney}
+            type='submit'
+            value='Add More Money'
+            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
           />
         </div>
       </form>
