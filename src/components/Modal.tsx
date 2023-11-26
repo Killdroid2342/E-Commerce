e90ba825dddf40d90594fb3047a3b9d284a5d774
Haroon Khan <HaroonKhan120704@gmail.com>
2023-11-26 00:06:56 +0000

    editing modal for users that have no account

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,22 @@
+import { decodeToken } from 'react-jwt';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 const Modal = ({ onClose, image, amount, add, remove, addBasket }: any) => {
-  const totalCost = 100 * amount;
+  const [clientUsername, setClientUsername] = useState('');
+  const isLoggedIn = !!clientUsername;
 
+  const totalCost = 100 * amount;
+  const usernameJWT = () => {
+    const getJWT = Cookies.get('UserjwtToken');
+    if (getJWT) {
+      const decodedTokenUsername = (decodeToken(getJWT) as { username: string })
+        .username;
+      setClientUsername(decodedTokenUsername);
+    } else return;
+  };
+  useEffect(() => {
+    usernameJWT();
+  }, []);
   return (
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black'>
       <div className='bg-white p-4 rounded-lg'>
@@ -11,28 +27,36 @@ const Modal = ({ onClose, image, amount, add, remove, addBasket }: any) => {
           X
         </button>
         <img src={image} alt='Modal' className='w-96' />
-        <h2 className='text-xl text-center'>Add To basket: £100</h2>
-        <div className='flex flex-col'>
-          <button
-            className='border-2 border-red-300 p-2 rounded-md font-bold m-2'
-            onClick={add}
-          >
-            {'+'}
-          </button>
-          <button
-            className='border-2 border-red-300 p-2 rounded-md font-bold m-2'
-            onClick={remove}
-          >
-            {'-'}
-          </button>
-          <button
-            onClick={addBasket}
-            className='border-2 border-red-300 p-2 rounded-md font-bold m-2'
-          >
-            Add To Basket
-          </button>
-        </div>
-        <p>{`Cost: £${totalCost}`}</p>
+        {isLoggedIn ? (
+          <>
+            <h2 className='text-xl text-center'>Add To basket: £100</h2>
+            <div className='flex flex-col'>
+              <button
+                className='border-2 border-red-300 p-2 rounded-md font-bold m-2'
+                onClick={add}
+              >
+                {'+'}
+              </button>
+              <button
+                className='border-2 border-red-300 p-2 rounded-md font-bold m-2'
+                onClick={remove}
+              >
+                {'-'}
+              </button>
+              <button
+                onClick={addBasket}
+                className='border-2 border-red-300 p-2 rounded-md font-bold m-2'
+              >
+                Add To Basket
+              </button>
+            </div>
+            <p>{`Cost: £${totalCost}`}</p>
+          </>
+        ) : (
+          <p className='text-center font-bold text-lg'>
+            To Buy This Item you need to make an account
+          </p>
+        )}
       </div>
     </div>
   );
