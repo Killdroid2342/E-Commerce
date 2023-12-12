965d1da72cc275c1178e36da6cc9983cd9391641
Haroon Khan <HaroonKhan120704@gmail.com>
2023-12-12 11:42:23 +0000

    aligned everything again

--- src/Pages/Accessories.tsx
+++ src/Pages/Accessories.tsx
@@ -73,30 +73,33 @@ const Accessories = () => {
   }, [basketItems, accessories]);
 
   return (
-    <div className='outerDiv'>
+    <div className='flex flex-col'>
       <Nav basketItems={basketItems} setBasketItems={setBasketItems} />
-      <div className='outerDescBox'>
-        <div className='mainBox'>
-          <h2 className='Heading'>Shoes</h2>
+      <div className='flex justify-center'>
+        <div className='mt-40 p-16 w-8/12 bg-neutral-200'>
+          <h2 className='text-4xl'>Shoes</h2>
           <p>
-            Buy and Sell Accessories on StockX. Every item is
-            <a className='pText'> StockX Verified.</a>
+            Buy and Sell Shoes on StockX. Every item is
+            <a className='text-green-900'> StockX Verified.</a>
           </p>
         </div>
       </div>
-      <div className='imgGrid'>
+      <div className='flex justify-center flex-row mt-20'>
         {accessories.slice(24, 28).map((image, index) => (
-          <div key={index} className='imgDiv'>
+          <div
+            key={index}
+            className='border border-neutral-400 m-10 cursor-pointer w-60'
+          >
             <img
               src={image.img}
               alt={`Product ${index}`}
-              className='mainImg'
+              className='w-56 h-48 border border-red-700'
               onClick={() => openModal(image.img)}
             />
-            <p className='imgName'>{image.name}</p>
+            <p className='font-bold'>{image.name}</p>
             <p>{image.des}</p>
-            <p className='imgLowPrice'>{image.lowPrice}</p>
-            <p className='imgPrice'>{image.price}</p>
+            <p className='text-xs'>{image.lowPrice}</p>
+            <p className='font-bold text-xl'>{image.price}</p>
           </div>
         ))}
         {modalI && (
@@ -110,19 +113,22 @@ const Accessories = () => {
           />
         )}
       </div>
-      <div className='imgGrid'>
+      <div className='flex justify-center flex-row mt-20'>
         {accessories.slice(28, 32).map((image, index) => (
-          <div key={index} className='imgDiv'>
+          <div
+            key={index}
+            className='border border-neutral-400 m-10 cursor-pointer w-60'
+          >
             <img
               src={image.img}
               alt={`Product ${index}`}
-              className='mainImg'
+              className='w-56 h-48 '
               onClick={() => openModal(image.img)}
             />
-            <p className='imgName'>{image.name}</p>
+            <p className='font-bold'>{image.name}</p>
             <p>{image.des}</p>
-            <p className='imgLowPrice'>{image.lowPrice}</p>
-            <p className='imgPrice'>{image.price}</p>
+            <p className='text-xs'>{image.lowPrice}</p>
+            <p className='font-bold text-xl'>{image.price}</p>
           </div>
         ))}
         {modalI && (
@@ -136,19 +142,22 @@ const Accessories = () => {
           />
         )}
       </div>
-      <div className='imgGrid'>
+      <div className='flex justify-center flex-row mt-20'>
         {accessories.slice(32, 36).map((image, index) => (
-          <div key={index} className='imgDiv'>
+          <div
+            key={index}
+            className='border border-neutral-400 m-10 cursor-pointer w-60'
+          >
             <img
               src={image.img}
               alt={`Product ${index}`}
-              className='mainImg'
+              className='w-56 h-48'
               onClick={() => openModal(image.img)}
             />
-            <p className='imgName'>{image.name}</p>
+            <p className='font-bold'>{image.name}</p>
             <p>{image.des}</p>
-            <p className='imgLowPrice'>{image.lowPrice}</p>
-            <p className='imgPrice'>{image.price}</p>
+            <p className='text-xs'>{image.lowPrice}</p>
+            <p className='font-bold text-xl'>{image.price}</p>
           </div>
         ))}
         {modalI && (
