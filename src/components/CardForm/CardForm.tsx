d768d76cf89d988c1b7d6662b2104b41612e4434
Haroon Khan <HaroonKhan120704@gmail.com>
2023-12-15 06:49:33 +0000

    made site responsive

--- src/components/CardForm/CardForm.tsx
+++ src/components/CardForm/CardForm.tsx
@@ -11,16 +11,20 @@ const CardForm = ({
   const openMoneyModal = () => {
     setMoneyModal(true);
   };
+
   return (
     <div className='flex flex-col items-center'>
-      <form className='border border-red-900 p-4 ml-10' onSubmit={submitCard}>
+      <form
+        className='border border-red-900 p-2 md:p-4 lg:p-6 xl:p-8 rounded-md max-w-md w-full mx-auto'
+        onSubmit={submitCard}
+      >
         <div className='mb-2 flex flex-col'>
           <label>FullName:</label>
           <input
             type='text'
             placeholder='John Doe'
             maxLength={16}
-            className='border border-neutral-500 p-2 focus:outline-none'
+            className='border border-neutral-500 focus:outline-none p-2 rounded-lg'
             name='FullName'
             onChange={cardInput}
           />
@@ -31,7 +35,7 @@ const CardForm = ({
             type='text'
             placeholder='1234 1234 1234 1234'
             maxLength={16}
-            className='border border-neutral-500 p-2 focus:outline-none'
+            className='border border-neutral-500 focus:outline-none p-2 rounded-lg'
             name='CardNumber'
             onChange={cardInput}
           />
@@ -42,7 +46,7 @@ const CardForm = ({
             type='text'
             placeholder='10/25'
             maxLength={5}
-            className='border border-neutral-500 p-2 focus:outline-none'
+            className='border border-neutral-500 focus:outline-none p-2 rounded-lg'
             name='ExpirationDate'
             onChange={cardInput}
           />
@@ -53,7 +57,7 @@ const CardForm = ({
             type='text'
             placeholder='123'
             maxLength={3}
-            className='border border-neutral-500 p-2 focus:outline-none'
+            className='border border-neutral-500 focus:outline-none p-2 rounded-lg'
             name='SecurityCode'
             onChange={cardInput}
           />
@@ -63,28 +67,29 @@ const CardForm = ({
           <input
             type='number'
             placeholder='£0'
-            className='border border-neutral-500 p-2 focus:outline-none appearance-none'
+            className='border border-neutral-500 focus:outline-none p-2 rounded-lg'
             name='Money'
             onChange={cardInput}
           />
         </div>
-        <div className='flex flex-row'>
+        <div className='max-sm:flex max-sm:flex-col flex flex-col'>
           <input
             type='submit'
             value='Create Card'
-            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
+            className='bg-blue-500 text-white p-1 md:p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
           />
           <input
             onClick={removeCard}
             type='submit'
             value='Remove Card'
-            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
+            className='bg-blue-500 text-white p-1 md:p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
           />
+
           <input
             onClick={openMoneyModal}
             type='button'
             value='Add More Money'
-            className='bg-blue-500 text-white p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
+            className='bg-blue-500 text-white p-1 md:p-2 rounded-lg cursor-pointer hover:bg-blue-700 m-2'
           />
         </div>
       </form>
