7072dad2bda2edd5db8ee90ff9cc3bdb98ec0944
Haroon Khan <HaroonKhan120704@gmail.com>
2024-01-11 07:44:14 +0000

    pagination for electronics

--- src/Pages/Electronics.tsx
+++ src/Pages/Electronics.tsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import Modal from '../components/Modal';
 import { Item } from './Shoes';
 import axios from 'axios';
+import Pagination from '../components/Pagination';
 const { VITE_API_URL } = import.meta.env;
 
 const instance = axios.create({
@@ -26,10 +27,18 @@ const Electronics = () => {
     initialBasketItems ? JSON.parse(initialBasketItems) : []
   );
   const [electronics, setElectronics] = useState<ElectronicsItems[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12;
 
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
   async function getElectronics() {
     const res = await instance.get('/items/allItems');
-    setElectronics(res.data);
+    const ElectronicItems = res.data.filter(
+      (item: any) => item.des === 'This is a Electronics'
+    );
+
+    setElectronics(ElectronicItems);
   }
   const openModal = (image: any) => {
     setModalI(image);
@@ -78,104 +87,47 @@ const Electronics = () => {
       <Nav basketItems={basketItems} setBasketItems={setBasketItems} />
       <div className='flex justify-center'>
         <div className='mt-40 p-16 w-8/12 bg-neutral-200'>
-          <h2 className='text-4xl'>Electronics</h2>
+          <h2 className='text-4xl'>Shoes</h2>
           <p>
-            Buy and Sell Electronics on StockX. Every item is
+            Buy and Sell Shoes on StockX. Every item is
             <a className='text-green-900'> StockX Verified.</a>
           </p>
         </div>
       </div>
-      <div className='flex justify-center'>
-        <div className='w-8/12'>
-          <div className='flex flex-row justify-between'>
-            {electronics.slice(12, 16).map((image, index) => (
-              <div
-                key={index}
-                className='border border-neutral-400 cursor-pointer w-60 mt-10'
-              >
-                <img
-                  src={image.img}
-                  alt={`Product ${index}`}
-                  className='w-56 h-48'
-                  onClick={() => openModal(image.img)}
-                />
-                <p className='font-bold'>{image.name}</p>
-                <p>{image.des}</p>
-                <p className='text-xs'>{image.lowPrice}</p>
-                <p className='font-bold text-xl'>{image.price}</p>
-              </div>
-            ))}
-            {modalI && (
-              <Modal
-                onClose={closeModal}
-                image={modalI}
-                amount={amount}
-                add={add}
-                remove={remove}
-                addBasket={addBasket}
-              />
-            )}
+      <div className='grid grid-cols-4 gap-4'>
+        {electronics.slice(startIndex, endIndex).map((image, index) => (
+          <div
+            key={index}
+            className='border border-neutral-400 cursor-pointer mt-10'
+          >
+            <img
+              src={image.img}
+              alt={`Product ${index}`}
+              className='w-56 h-48'
+              onClick={() => openModal(image.img)}
+            />
+            <p className='font-bold'>{image.name}</p>
+            <p>{image.des}</p>
+            <p className='text-xs'>{image.lowPrice}</p>
+            <p className='font-bold text-xl'>{image.price}</p>
           </div>
-          <div className='flex flex-row justify-between'>
-            {electronics.slice(16, 20).map((image, index) => (
-              <div
-                key={index}
-                className='border border-neutral-400 mt-10 cursor-pointer w-60'
-              >
-                <img
-                  src={image.img}
-                  alt={`Product ${index}`}
-                  className='w-56 h-48 '
-                  onClick={() => openModal(image.img)}
-                />
-                <p className='font-bold'>{image.name}</p>
-                <p>{image.des}</p>
-                <p className='text-xs'>{image.lowPrice}</p>
-                <p className='font-bold text-xl'>{image.price}</p>
-              </div>
-            ))}
-            {modalI && (
-              <Modal
-                onClose={closeModal}
-                image={modalI}
-                amount={amount}
-                add={add}
-                remove={remove}
-                addBasket={addBasket}
-              />
-            )}
-          </div>
-          <div className='flex flex-row justify-between'>
-            {electronics.slice(20, 24).map((image, index) => (
-              <div
-                key={index}
-                className='border border-neutral-400 mt-10 mb-10 cursor-pointer w-60'
-              >
-                <img
-                  src={image.img}
-                  alt={`Product ${index}`}
-                  className='w-56 h-48'
-                  onClick={() => openModal(image.img)}
-                />
-                <p className='font-bold'>{image.name}</p>
-                <p>{image.des}</p>
-                <p className='text-xs'>{image.lowPrice}</p>
-                <p className='font-bold text-xl'>{image.price}</p>
-              </div>
-            ))}
-            {modalI && (
-              <Modal
-                onClose={closeModal}
-                image={modalI}
-                amount={amount}
-                add={add}
-                remove={remove}
-                addBasket={addBasket}
-              />
-            )}
-          </div>
-        </div>
+        ))}
+        {modalI && (
+          <Modal
+            onClose={closeModal}
+            image={modalI}
+            amount={amount}
+            add={add}
+            remove={remove}
+            addBasket={addBasket}
+          />
+        )}
       </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(electronics.length / itemsPerPage)}
+        onPageChange={(page: any) => setCurrentPage(page)}
+      />
       <Footer />
     </div>
   );
